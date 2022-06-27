3dd016147c3f2b83a3a876352570113fe4543a9f
princevarshney05 <princevarshney05@gmail.com>
2022-06-27 13:41:22 +0530

    removed support for python 2

--- src/pages/data/languages.js
+++ src/pages/data/languages.js
@@ -2,7 +2,6 @@ const languagesToId = {
   "C (GCC 9.2.0)": 50,
   "C++ (GCC 9.2.0)": 54,
   "Java (OpenJDK 13.0.1)": 62,
-  "Python (2.7.17)": 70,
   "Python (3.8.1)": 71,
 };
 
@@ -10,7 +9,6 @@ const languagesToMonacoId = {
   "C (GCC 9.2.0)": "c",
   "C++ (GCC 9.2.0)": "cpp",
   "Java (OpenJDK 13.0.1)": "java",
-  "Python (2.7.17)": "python",
   "Python (3.8.1)": "python",
 };
 
