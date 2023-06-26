273ccdac971f0d9b9aecf4f888afc76cdea33881
gnaneswar-k <gnanu2004@gmail.com>
2023-06-26 16:42:42 +0530

    Updated backend link

--- src/pages/api.js
+++ src/pages/api.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const client = axios.create({
   // baseURL: "https://lepkbux71j.execute-api.ap-south-1.amazonaws.com/Prod",
-  baseURL: "http://127.0.0.1:3000", //Fact url localhost. Replace with amazon aws link.
+  // baseURL: "http://127.0.0.1:3000", //Fact url localhost. Replace with amazon aws link.
+  baseURL: "https://3k08izf70i.execute-api.ap-south-2.amazonaws.com/Prod",
 });
 
 export default client;
