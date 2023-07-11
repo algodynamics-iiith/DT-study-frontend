2e0ed0ad52502d4912fc5ed062c0dc73c2474cb8
gnaneswar-k <gnanu2004@gmail.com>
2023-07-11 12:50:01 +0530

    Updated links.

--- src/pages/data/paths.js
+++ src/pages/data/paths.js
@@ -4,9 +4,9 @@ const paths = [
 ];
 
 const drvingTestUrl = {
+  // 3: "http://localhost:3001?id=" // For local testing. Change to link after uploading frontend.
   1: "https://algodynamics-iiith.github.io/bubble/public/experiments/BubbleSortTest.html?id=",
   2: "https://algodynamics-iiith.github.io/bubble/public/experiments/HeapifyTest.html?id=",
-  // 3: "http://localhost:3001?id=" //change to github link after uploading frontend
   3: "https://gnaneswar-k.github.io/algodynamics-factorial?id=",
 };
 
